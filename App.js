b224b6b44e03ac785c407c514b8f97ac0aa5eb4b
mac <mohammedquraysh4@gmail.com>
2022-04-26 23:15:10 +0100

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, StyleSheet, Text, TextInput, ToastAndroid, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const[lang, Setlang]= useState('')
+  const[submitted, setSubmitted] = useState(false)
+  const onClickHandler=()=>{
+    if(lang.length > 3){
+      setSubmitted(!submitted)
+    }else if(lang.length == 2){
+      Alert.alert('Warning', 'You entered an invalid Languange', [
+        {
+          text: 'Ok', onPress:()=> console.warn('Okay Pressed')
+        },
+      {
+        text: 'Continue', onPress:()=> console.warn('Continue Pressed')
+      },
+      {
+        text: 'Cancel', onPress:()=> console.warn('cancel was Pressed')
+      }
+    ],{
+      cancelable: true, onDismiss:()=>console.warn('Alert dismssed')}
+      )
+    }else{
+      // ToastAndroid.show('Invalid', ToastAndroid.LONG)
+      // ToastAndroid.showWithGravity('Invalid Lang', ToastAndroid.SHORT, ToastAndroid.TOP)
+      ToastAndroid.showWithGravityAndOffset('error message',ToastAndroid.LONG, ToastAndroid.BOTTOM, 600,0)
+    }
+      
+  }
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Text style={styles.text}>Which language are you comfortable with?</Text>
+      <TextInput
+      style={styles.textInput}
+      onChangeText={(value)=>Setlang(value)}
+      placeholder='e.g Kotlin, JavaScript'
+      />
+      <Text>Your preferred language is: {lang}</Text>
+      <TouchableOpacity
+      style={styles.touchableButtton}
+      onPress={onClickHandler}
+      >
+        <Text>{submitted ? 'submit' : 'clear'}</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +57,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput:{
+    width: 200,
+    borderWidth:1,
+    textAlign: 'center', 
+    marginBottom: 10
+  },
+  text:{
+    margin:20,
+  },
+  touchableButtton:{
+    backgroundColor:'#08F',
+    borderRadius: 5,
+    margin: 20,
+    width: 50,
+    alignItems:'center',
+  }
 });
